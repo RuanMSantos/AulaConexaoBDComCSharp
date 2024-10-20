7483899a25fb1ecb11554c6634e5a079c27484ed
RuanMSantos <ruancauancauan@gmail.com>
2024-10-19 23:52:51 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using AulaConexaoBDComCSharp.db;
+
+Console.WriteLine("Tarefas encontradas:");
+
+int numeroTarefas = 0;
+
+using (var _db = new TarefasContext()){
+    
+    var tarefas = _db.Tarefa;
+
+    numeroTarefas = tarefas.Count();
+}
+
+
+Console.WriteLine($"Foram encontradas {numeroTarefas} tarefas.");
